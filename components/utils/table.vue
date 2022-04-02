6b40afd2a42bbc3b8df27ad8215d9abbfc87162a
JoaoPedroMDP1 <dev.jpmpjp00@gmail.com>
2022-04-02 19:25:24 -0300

    [add] Componente de tabela [add] Página para listar novas entradas, com dados Dummy. Pode ser acessada com "localhost.../management/newEntries"

---
+++ components/utils/table.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    :sort-by="['calories', 'fat']"
+    :sort-desc="[false, true]"
+    multi-sort
+    class="elevation-1"
+  ></v-data-table>
+</template>
+<script>
+export default {
+  name: 'Table',
+  props: {
+    headers: {
+      type: Array,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
